e56f29216e27ea335b4b5ccf110239afbbb7ba0c
Mikael Mortensen <mikaem@math.uio.no>
2016-04-13 08:57:28 +0200

    Fixing demo spectral solver with new FFT

--- demo/spectralDNSsolver.py
+++ demo/spectralDNSsolver.py
@@ -1,5 +1,5 @@
 __author__ = "Mikael Mortensen <mikaem@math.uio.no>"
-__date__ = "2016-02-26"
+__date__ = "2016-04-07"
 __copyright__ = "Copyright (C) 2016 " + __author__
 __license__  = "GNU Lesser GPL version 3 or any later version"
 
@@ -30,34 +30,35 @@ P_hat = empty(FFT.complex_shape(), dtype=complex)
 U_hat0  = empty((3,) + FFT.complex_shape(), dtype=complex)
 U_hat1  = empty((3,) + FFT.complex_shape(), dtype=complex)
 dU      = empty((3,) + FFT.complex_shape(), dtype=complex)
-curl    = empty((3,) + FFT.real_shape())
 X = FFT.get_local_mesh()
 K = FFT.get_scaled_local_wavenumbermesh()
 K2 = sum(K*K, 0, dtype=float)
 K_over_K2 = K.astype(float) / where(K2==0, 1, K2).astype(float)    
-dealias = FFT.get_dealias_filter()
 a = [1./6., 1./3., 1./3., 1./6.]
 b = [0.5, 0.5, 1.]
+dealias = '3/2-rule'  # ('2/3-rule', None)
 
 def Cross(a, b, c):
-    c[0] = FFT.fftn(a[1]*b[2]-a[2]*b[1], c[0])
-    c[1] = FFT.fftn(a[2]*b[0]-a[0]*b[2], c[1])
-    c[2] = FFT.fftn(a[0]*b[1]-a[1]*b[0], c[2])
+    c[0] = FFT.fftn(a[1]*b[2]-a[2]*b[1], c[0], dealias=dealias)
+    c[1] = FFT.fftn(a[2]*b[0]-a[0]*b[2], c[1], dealias=dealias)
+    c[2] = FFT.fftn(a[0]*b[1]-a[1]*b[0], c[2], dealias=dealias)
     return c
 
 def Curl(a, c):
-    c[2] = FFT.ifftn(1j*(K[0]*a[1]-K[1]*a[0]), c[2])
-    c[1] = FFT.ifftn(1j*(K[2]*a[0]-K[0]*a[2]), c[1])
-    c[0] = FFT.ifftn(1j*(K[1]*a[2]-K[2]*a[1]), c[0])
+    c[2] = FFT.ifftn(1j*(K[0]*a[1]-K[1]*a[0]), c[2], dealias=dealias)
+    c[1] = FFT.ifftn(1j*(K[2]*a[0]-K[0]*a[2]), c[1], dealias=dealias)
+    c[0] = FFT.ifftn(1j*(K[1]*a[2]-K[2]*a[1]), c[0], dealias=dealias)
     return c
 
+work_shape = FFT.real_shape_padded() if dealias == '3/2-rule' else FFT.real_shape()
 def computeRHS(dU, rk):
-    if rk > 0:
-        for i in range(3):
-            U[i] = FFT.ifftn(U_hat[i], U[i])
-    curl[:] = Curl(U_hat, curl)
-    dU = Cross(U, curl, dU)
-    dU *= dealias
+    U_dealiased = FFT.get_workarray(((3,) + work_shape, float), 0)
+    curl_dealiased = FFT.get_workarray(((3,) + work_shape, float), 1)
+    for i in range(3):
+        U_dealiased[i] = FFT.ifftn(U_hat[i], U_dealiased[i], dealias)
+
+    curl_dealiased[:] = Curl(U_hat, curl_dealiased)
+    dU = Cross(U_dealiased, curl_dealiased, dU)
     P_hat[:] = sum(dU*K_over_K2, 0, out=P_hat)
     dU -= P_hat*K
     dU -= nu*K2*U_hat
@@ -82,8 +83,9 @@ while t < T-1e-8:
             U_hat[:] = U_hat0 + b[rk]*dt*dU
         U_hat1[:] += a[rk]*dt*dU
     U_hat[:] = U_hat1[:]
-    for i in range(3):
-        U[i] = FFT.ifftn(U_hat[i], U[i])
+
+for i in range(3):
+    U[i] = FFT.ifftn(U_hat[i], U[i])
 
 k = FFT.comm.reduce(sum(U*U)/N[0]/N[1]/N[2]/2)
 if FFT.rank == 0:
