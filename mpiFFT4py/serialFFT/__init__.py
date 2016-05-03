4e6847fc73d87a632ff0175b34704c09f7c04fe3
Nathanael Schilling <nathanaelschilling@gmx.net>
2016-05-03 11:25:24 +0300

    Warn if using numpy instead of pyfftw

--- mpiFFT4py/serialFFT/__init__.py
+++ mpiFFT4py/serialFFT/__init__.py
@@ -1,4 +1,6 @@
+import warnings
 try:
     from pyfftw_fft import *
 except:
+    warnings.warn("Using numpy for FFTs, this is probably slower than using pyfftw and multithreading is not supported")
     from numpy_fft import *
