5a2658d7e069861f72d0006cc11faf27d7069853
Tobias Heinemann <tobias.heinemann@gmail.com>
2016-07-29 16:47:57 +0200

    Made relative imports Python 3 compatible

--- mpiFFT4py/__init__.py
+++ mpiFFT4py/__init__.py
@@ -1,6 +1,6 @@
-from serialFFT import *
-from slab import R2C as Slab_R2C
-from pencil import R2C as Pencil_R2C
-from line import R2C as Line_R2C
-from mpibase import work_arrays, datatypes
+from .serialFFT import *
+from .slab import R2C as Slab_R2C
+from .pencil import R2C as Pencil_R2C
+from .line import R2C as Line_R2C
+from .mpibase import work_arrays, datatypes
 
