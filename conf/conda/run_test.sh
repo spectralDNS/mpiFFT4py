ae49808b659ee894692d5ce6823429e8d848f2c5
Mikael Mortensen <mikaem@math.uio.no>
2017-02-06 12:15:42 +0100

    Syntax error in run_tests

--- conf/conda/run_test.sh
+++ conf/conda/run_test.sh
@@ -2,11 +2,11 @@
 
 pushd tests
 
-if [ "${CONDA_PY:0:1}" == "3" ]
+if [ "${CONDA_PY:0:1}" == "3" ]; then
     mpirun -np 4 py.test
 fi
 
-if [ "${CONDA_PY:0:1}" == "2" ]
+if [ "${CONDA_PY:0:1}" == "2" ]; then
     mpirun -np 1 py.test
 fi
 
