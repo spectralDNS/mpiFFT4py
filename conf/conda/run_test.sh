80d36b795c0290dfbf0a29695b65a8f266fc3137
Mikael Mortensen <mikaem@math.uio.no>
2017-02-08 11:02:44 +0100

    Attempt parallel for osx

--- conf/conda/run_test.sh
+++ conf/conda/run_test.sh
@@ -3,11 +3,11 @@
 pushd tests
 
 if [ "$(uname)" == "Darwin" ]; then
-    py.test -v
+    mpirun -np 4 py.test -v
 fi
 
 if [ "$(uname)" == "Linux" ]; then
-    mpirun -np 4 py.test
+    mpirun -np 4 py.test -v
 fi
 # if [ "${CONDA_PY:0:1}" == "3" ]; then
 #     mpirun -np 4 py.test
