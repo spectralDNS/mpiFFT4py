9cf6e9c31d8b650607e5ba09a74b16169f8b3646
Mikael Mortensen <mikaem@math.uio.no>
2017-02-08 09:20:13 +0100

    Use only one cpu for travis test

--- conf/conda/run_test.sh
+++ conf/conda/run_test.sh
@@ -2,11 +2,18 @@
 
 pushd tests
 
-if [ "${CONDA_PY:0:1}" == "3" ]; then
-    mpirun -np 4 py.test
-fi
-
-if [ "${CONDA_PY:0:1}" == "2" ]; then
+if [ "$(uname)" == "Darwin" ]; then
     mpirun -np 1 py.test
 fi
 
+if [ "$(uname)" == "Linux" ]; then
+    mpirun -np 4 py.test
+fi
+# if [ "${CONDA_PY:0:1}" == "3" ]; then
+#     mpirun -np 4 py.test
+# fi
+#
+# if [ "${CONDA_PY:0:1}" == "2" ]; then
+#     mpirun -np 1 py.test
+# fi
+#
